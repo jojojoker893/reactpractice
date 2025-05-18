02950f5c11d7792d6b61b40ed124006be963e51e
jojojoker893 <jojojoker114514@gmail.com>
2025-05-18 22:31:16 +0900

    add: コンポーネントへuseStateを追加

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,3 +1,19 @@
+import { useState } from 'react';
+
 export default function Square() {
-  return <button className="square">1</button>;
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
+
